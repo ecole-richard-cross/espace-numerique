3c5366331bf0c49e39545eda9eaebc4b08f3d44f
Salomé <131461004+SalomeVv@users.noreply.github.com>
2023-12-12 17:01:42 +0100

    feat/profile-page: avatar upload

---
+++ src/Form/AvatarType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form\Type;
+
+use App\Entity\Media;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image;
+
+class AvatarType extends AbstractType
+{
+   public function buildForm(FormBuilderInterface $builder, array $options): void
+   {
+      $builder
+         ->add('image', FileType::class, [
+            'label' => false,
+            'mapped' => false,
+            'required'=> false,
+            'constraints' => [
+               new Image([
+                  'maxSize' => '3000k',
+                  'maxSizeMessage' => 'Le fichier est trop large ({{ size }} {{ suffix }}). Taille maximale {{ limit }} {{ suffix }}.',
+                  'mimeTypesMessage' => 'Ce fichier n\'est pas une image.',
+               ])
+            ],
+         ])
+
+         ->add('type', HiddenType::class, [
+            'data' => 'image',
+         ])
+         ->add('submit', SubmitType::class, [
+            'label' => 'Confirmer'
+         ]);
+   }
+
+   public function configureOptions(OptionsResolver $resolver)
+   {
+      $resolver->setDefaults([
+         'data_class' => Media::class,
+      ]);
+   }
+}
