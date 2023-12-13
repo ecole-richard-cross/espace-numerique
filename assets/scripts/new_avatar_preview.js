e4d6fc07f7a9395ce15e52134ae49200358f5340
LeFreaky <100714646+LeFreaky@users.noreply.github.com>
2023-12-13 16:30:33 +0100

    fix/dev : Avatar
    Used current avatar rather than placeholder for avatar form and added accept attribute to file input field

--- assets/scripts/new_avatar_preview.js
+++ assets/scripts/new_avatar_preview.js
@@ -1,9 +1,9 @@
-const previewFallback = document.querySelector('#avatarPreview svg');
+// const previewFallback = document.querySelector('#avatarPreview svg');
 const previewImage = document.querySelector('#avatarPreview img');
 const imageInput = document.getElementById('avatar_image');
 
 imageInput.addEventListener("change", (e) => {
-   previewFallback.classList.add('d-none');
+   // previewFallback.classList.add('d-none');
    previewImage.src = window.URL.createObjectURL(e.target.files[0]);
-   previewImage.classList.remove('d-none');
+   // previewImage.classList.remove('d-none');
 })
